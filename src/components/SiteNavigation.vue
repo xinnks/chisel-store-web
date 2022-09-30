d8554479581a205daf0d8f3622795e682da86cd1
James Sinkala <xinnks@gmail.com>
2022-09-30 10:13:08 +0300

    feat: Add a site navigation component

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Cart from '../components/Cart.vue'
+</script>
+
+<template>
+  <nav class="flex justify-between py-4 m-2 px-12">
+    <!-- logo -->
+    <div class="flex flex-col justify-center py-2">
+      <span class="text-lg md:text-xl lg:text-2xl font-extrabold">CHISEL STORE</span>
+    </div>
+
+    <!-- nav items -->
+    <div class="flex flex-col justify-center">
+      <ul class="p-2 flex space-x-3 text-xl font-semibold">
+        <li>LINK-ONE</li>
+        <li>LINK-TWO</li>
+        <li>LINK-THREE</li>
+      </ul>
+    </div>
+
+    <!-- cart -->
+    <Cart></Cart>
+    <!-- cart -->
+  </nav>
+</template>
