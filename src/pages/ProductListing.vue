22f124f8494a5e221348eafe92e8bd5f369de727
James Sinkala <xinnks@gmail.com>
2022-10-02 11:57:43 +0300

    feat: Implement site search and product listing filters logic

--- src/pages/ProductListing.vue
+++ src/pages/ProductListing.vue
@@ -5,6 +5,7 @@ import { onMounted, ref, watch, computed } from 'vue';
 import axios from 'axios';
 let products = ref([]);
 let categories = ref([]);
+let contentFetchFilters = ref ({});
 let nextPageFilter = ref([]);
 let prevPageFilter = ref([]);
 /**
@@ -36,11 +37,82 @@ const fetchCategories = async () => {
   }
 }
 
+/**
+ * @description Converts the filters object into a url compatible string
+ * @param {Object} filters
+ */
+const stringifyFilters = (filters) => Object.keys(filters).map(key => `${key}=${filters[key]}`).join('&');
+
 // Run functions when the component is mounted
 onMounted(() => {
   fetchProducts()
   fetchCategories()
 })
+
+const resetAllOtherFilters = () => {
+  contentFetchFilters.value = {}
+  selectedCategory.value = 'all'
+  price.value = {start: "", end: ""}
+}
+
+
+// FILTERS
+// * Chiselstrike: GET method endpoint filters, gt, gte, lt, lte
+
+let selectedCategory = ref('all');
+let price = ref({start: "", end: ""})
+
+// Watch for changes in the selectedCategory reactive variable
+watch(selectedCategory, async (newCategory) => {
+  let currentFiltersQuery = contentFetchFilters.value, newCategoryFilter;
+
+  // update route with the new category filter selected
+  if(newCategory !== "all") {
+    newCategoryFilter = Object.assign(currentFiltersQuery, {'.category.name': newCategory})
+  } else {
+    delete currentFiltersQuery['.category.name']
+    newCategoryFilter = currentFiltersQuery;
+  }
+  contentFetchFilters.value = newCategoryFilter;
+  let newRequestFilters = stringifyFilters(newCategoryFilter);
+  fetchProducts(newRequestFilters)
+})
+
+// Updates the price related filters for products to be listed
+const updatePrices = () => {
+  let currentFiltersQuery = contentFetchFilters.value, priceFilter;
+
+  if(price.value.from) {
+    priceFilter = Object.assign(currentFiltersQuery, {'.price~gte': parseFloat(price.value.from)})
+  } else {
+    if(priceFilter) delete priceFilter['.price~gte']
+  }
+  if(price.value.to) {
+    priceFilter = Object.assign(currentFiltersQuery, {'.price~lte': parseFloat(price.value.to)})
+  } else {
+    if(priceFilter) delete priceFilter['.price~lte']
+  }
+  if(price.value.from || price.value.to){
+    contentFetchFilters.value = priceFilter;
+    let newRequestFilters = stringifyFilters(priceFilter);
+    fetchProducts(newRequestFilters)
+  }
+}
+
+// SITE SEARCH 
+// * IMPORTANT: Chiselstrike SQL like "like" filter implementation
+
+let searchQuery = ref('')
+
+/**
+ * @description Performs a search
+ */
+const search = () => {
+  resetAllOtherFilters();
+  if(searchQuery.value && searchQuery.value.length >= 3){
+    fetchProducts(`?.name~like=%${searchQuery.value}%&.details~like=%${searchQuery.value}%`)
+  }
+}
 
 // PAGINATION
 // * Chiselstrike: next_page and prev_page from get endpoints
